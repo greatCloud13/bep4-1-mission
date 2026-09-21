66536aa0839d01d689ba1435ebc8e72f641867f9
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2026-09-21 10:53:43 +0900

    0029 commit

--- src/main/java/com/back/boundedContext/member/app/MemberSupport.java
+++ src/main/java/com/back/boundedContext/member/app/MemberSupport.java
@@ -2,29 +2,18 @@ package com.back.boundedContext.member.app;
 
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.member.out.MemberRepository;
-import com.back.global.exception.DomainException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class MemberService {
+@RequiredArgsConstructor
+public class MemberSupport {
     private final MemberRepository memberRepository;
-
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     public long count() {
         return memberRepository.count();
-    }
-
-    public Member join(String username, String password, String nickname) {
-        findByUsername(username).ifPresent(m -> {
-            throw new DomainException("409-1", "이미 존재하는 username 입니다.");
-        });
-
-        return memberRepository.save(new Member(username, password, nickname));
     }
 
     public Optional<Member> findByUsername(String username) {
